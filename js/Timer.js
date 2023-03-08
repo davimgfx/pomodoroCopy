21e2864e9df7e2e1f3862eaf0cacb3fcab561cd1
davimgfx <davifncosta@gmail.com>
2023-03-08 15:10:05 -0300

    add the function of countdown

--- js/Timer.js
+++ js/Timer.js
@@ -10,12 +10,20 @@ export default class Timer {
 		};
 
 		this.interval = null;
-		this.remainingSeconds = 50*60;
+		this.remainingSeconds = 30;
+	
+		this.el.control.addEventListener("click", () => {
+			if(this.interval === null){
+				this.start()
+			} else{
+				this.stop()
+			}
+		});
 
-		this.updateInterfaceTime();
-
-		this.el.control.addEventListener("click", () => {});
-		this.el.reset.addEventListener("click", () => {});
+		this.el.reset.addEventListener("click", () => {
+			this.remainingSeconds = 30
+			this.updateInterfaceTime()
+		});
 	}
 
 	updateInterfaceTime() {
@@ -26,7 +34,40 @@ export default class Timer {
 		this.el.seconds.textContent = seconds.toString().padStart(2, "0");
 	}
 
-	updateInterfaceControl() {}
+	updateInterfaceControl() {
+		if (this.interval === null) {
+			this.el.control.innerHTML = `<button class="button" class="timer__btn--control timer__btn--start">START</button>`;
+			this.el.control.class.add("timer__brn--start");
+			this.el.control.class.remove("timer__brn--stop");
+		} else {
+			this.el.control.innerHTML = `<button class="button" class="timer__btn--control timer__btn--start">PAUSE</button>`;
+			this.el.control.class.add("timer__btn--stop");
+			this.el.control.class.remove("timer__brn--start");
+		}
+	}
+
+	start() {
+		if (this.remainingSeconds === 0) return;
+
+		this.interval = setInterval(() => {
+			this.remainingSeconds--;
+			this.updateInterfaceTime();
+
+			if (this.remainingSeconds == 0) {
+				this.stop();
+			}
+		}, 1000);
+
+		this.updateInterfaceControl();
+	}
+
+	stop() {
+		clearInterval(this.interval);
+
+		this.interval = null;
+
+		this.updateInterfaceControl();
+	}
 
 	static getHTML() {
 		return `
@@ -39,10 +80,12 @@ export default class Timer {
                 </ul>
             </nav>
 			<div class="timer__clock">
-                <span class="timer__part timer__part--minutes">00</span><span class="timer__part">:</span><span class="timer__part timer__part--seconds">00</span>
+                <span class="timer__part timer__part--minutes">50</span><span class="timer__part">:</span><span class="timer__part timer__part--seconds">00</span>
             </div>
-			<button id="button" class="timer__btn--control timer__btn--start">START</button>
-            <button type="button" class="timer__btn timer__btn--reset">RESET</button>
+			<div class="controls">
+			<button class="timer__btn--control timer__btn--start button">START</button>
+            <button type="button" class="timer__btn timer__btn--reset button">RESET</button>
+			</div>
 		`;
 	}
 }
