aa81eaf2e5f0215ddb000fe86713e5603c7f303e
Davi Costa <davifncosta@gmail.com>
2023-04-06 08:42:36 -0300

    final project ✨🎉🎉

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 "use strict";
 import Timer from "./Timer.js";
-import Navbar from "./Navbar.js";
-
 new Timer(document.querySelector(".timer"));
-new Navbar(document.querySelector(".header__navbar"));
